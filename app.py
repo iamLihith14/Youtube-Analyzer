89612bda3eb3cbca366d72b3f1cd1403babc5f7c
lihith-n <142425444+iamLihith14@users.noreply.github.com>
2024-12-16 20:50:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 from textblob import TextBlob
 
 # Set your YouTube Data API key here
-YOUTUBE_API_KEY = "AIzaSyD7OUR71LzrVXntYUXlSjUxv1ZCkjNYpGM"
+YOUTUBE_API_KEY = "AIzaSyCkTzZjYyrTgKhTRX-bmfipYJQIVr8Cn4k"
 
 # Initialize the YouTube Data API client
 youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
